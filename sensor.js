2e94c91aef516341a6e637b6a0f00e2419c4edb5
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-15 20:24:42 +0530

    Cleanup

--- sensor.js
+++ sensor.js
@@ -1,14 +1,18 @@
 class Sensor {
   constructor(car) {
     this.car = car;
-    this.rayCount = 3;
-    this.rayLength = 100;
+    this.rayCount = 5;
+    this.rayLength = 150;
     this.raySpread = Math.PI / 4;
 
     this.rays = [];
   }
 
   update() {
+    this.#castRays();
+  }
+
+  #castRays() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
       const rayAngle =
