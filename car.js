17b40a7df35b754e0d85aa35ed43414c600e0de3
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-15 16:12:02 +0530

    Add controls

--- car.js
+++ car.js
@@ -8,6 +8,21 @@ class Car {
     this.controls = new Controls();
   }
 
+  update() {
+    if (this.controls.forward) {
+      this.y -= 2;
+    }
+    if (this.controls.reverse) {
+      this.y += 2;
+    }
+    if (this.controls.left) {
+      this.x -= 2;
+    }
+    if (this.controls.right) {
+      this.x += 2;
+    }
+  }
+
   draw(ctx) {
     ctx.beginPath();
     ctx.rect(
