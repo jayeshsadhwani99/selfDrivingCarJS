d11f73d7505666f47ada573a4af5d525c5bbf766
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-15 16:19:45 +0530

    Fix speed bug

--- car.js
+++ car.js
@@ -33,6 +33,9 @@ class Car {
     if (this.speed < 0) {
       this.speed += this.friction;
     }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
     this.y -= this.speed;
   }
 
