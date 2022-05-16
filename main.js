7efe69453b9e6f4c2219359e387ed71006171808
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-16 16:09:38 +0530

    Parallelization

--- main.js
+++ main.js
@@ -8,38 +8,60 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 8);
+
+const N = 100;
+const cars = generateCars(N);
 
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")];
 
-car.draw(carCtx);
-
 animate();
+
+function generateCars(N) {
+  const cars = [];
+  for (let i = 0; i < N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), -100, 30, 50, "AI"));
+  }
+
+  return cars;
+}
 
 function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
-  car.update(road.borders, traffic);
+
+  for (let i = 0; i < cars.length; i++) {
+    const car = cars[i];
+    car.update(road.borders, traffic);
+  }
   carCanvas.height = window.innerHeight;
 
   networkCanvas.height = window.innerHeight;
 
   carCtx.save();
-  carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+  carCtx.translate(0, -cars[0].y + carCanvas.height * 0.7);
 
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].draw(carCtx, "red");
   }
 
   road.draw(carCtx);
-  car.draw(carCtx, "blue");
+
+  carCtx.globalAlpha = 0.2;
+
+  for (let i = 0; i < cars.length; i++) {
+    const car = cars[i];
+    car.draw(carCtx, "blue");
+  }
+
+  carCtx.globalAlpha = 1;
+  cars[0].draw(carCtx, "blue", true);
 
   carCtx.restore();
 
   networkCtx.lineDashOffset = -time / 50;
 
   // Visualize neural network
-  Visualizer.drawNetwork(networkCtx, car.brain);
+  Visualizer.drawNetwork(networkCtx, cars[0].brain);
   requestAnimationFrame(animate);
 }
