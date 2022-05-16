658bba94b326ccbc89eac1945e4a669926c839f3
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-16 19:35:02 +0530

    save car to localstorage

--- main.js
+++ main.js
@@ -11,10 +11,24 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+
+// Check if there is already a best brain
+if (localStorage.getItem("bestBrain")) {
+  bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+}
 
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")];
 
 animate();
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N) {
   const cars = [];
@@ -35,7 +49,7 @@ function animate(time) {
     car.update(road.borders, traffic);
   }
 
-  const bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
+  bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
 
   carCanvas.height = window.innerHeight;
 
